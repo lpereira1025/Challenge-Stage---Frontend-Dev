fe920dc3a0c3f411353d2e3899ba01a1a655f944
Lucas Pereira <lpereira349@gmail.com>
2025-02-05 20:45:36 -0300

    First commit, add dependencies, server and pages

---
+++ src/app/events/list-events/list-events.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-events',
+  imports: [],
+  templateUrl: './list-events.component.html',
+  styleUrl: './list-events.component.css'
+})
+export class ListEventsComponent {
+
+}
